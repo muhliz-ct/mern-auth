7688d574decf835fb8515818bd6762526b64fc7f
muhliz <muhamedmuhlisct@gmail.com>
2024-07-30 15:07:03 +0530

    add changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import About from "./Pages/About"
+import Signin from "./Pages/Signin"
+import Signup from "./Pages/Signup"
+import Profile from "./Pages/Profile"
+import Home from "./Pages/Home"
 
 export default function App() {
-  return (
-    <div className='text-red-500'>
-      App
-    </div>
-  )
+  return <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/sign-in" element={<Signin />} />
+      <Route path="/sign-up" element={<Signup />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
+  </BrowserRouter>
 }
